09dd22ae4b8046a02165580f9b75144aa549a608
David López Castellote <david.lopez.castellote@gmail.com>
2020-06-04 17:14:26 +0200

    pantallas de pausa, game over, comenzar

--- js/app.js
+++ js/app.js
@@ -4,24 +4,41 @@ const lTetromino = [
     /* Según la foto "tetrominios.jpg", teniendo en cuenta que cada fila tiene 10 cuadrados,
         definimos en cada uno de los arrays las posiciones (de 0 a 299) de las celdas de la figura
     */
-    [1, rowCount+1, rowCount*2+1, 2],
-    [rowCount, rowCount+1, rowCount+2, rowCount*2+2],
-    [1, rowCount+1, rowCount+1, rowCount*2, rowCount*2+1],
-    [rowCount, rowCount*2, rowCount*2+1, rowCount*2+2]
+    [0, rowCount, rowCount*2, rowCount*2+1],
+    [0,1,2,rowCount],
+    [0,1,rowCount+1,rowCount*2+1],
+    [2,rowCount,rowCount+1,rowCount+2]
+];
+const jTetromino = [
+    [1,rowCount+1,rowCount*2+1,rowCount*2],
+    [0,rowCount,rowCount+1,rowCount+2],
+    [0,1,rowCount,rowCount*2],
+    [0,1,2,rowCount+2]
 ];
 const zTetromino = [
+    [0,1,rowCount+1,rowCount+2],
+    [1,rowCount,rowCount+1,rowCount*2],
+    [0,1,rowCount+1,rowCount+2],
+    [1,rowCount,rowCount+1,rowCount*2]
+];
+const sTetromino = [
+    [1,2,rowCount,rowCount+1],
     [0,rowCount,rowCount+1,rowCount*2+1],
-    [rowCount+1, rowCount+2,rowCount*2,rowCount*2+1],
-    [0,rowCount,rowCount+1,rowCount*2+1],
-    [rowCount+1, rowCount+2,rowCount*2,rowCount*2+1]
+    [1,2,rowCount,rowCount+1],
+    [0,rowCount,rowCount+1,rowCount*2+1]
+];
+const iTetromino = [
+    [1,rowCount+1,rowCount*2+1,rowCount*3+1],
+    [rowCount,rowCount+1,rowCount+2,rowCount+3],
+    [1,rowCount+1,rowCount*2+1,rowCount*3+1],
+    [rowCount,rowCount+1,rowCount+2,rowCount+3],
 ];
 const tTetromino = [
-    [1,rowCount,rowCount+1,rowCount+2],
-    [1,rowCount+1,rowCount+2,rowCount*2+1],
-    [rowCount,rowCount+1,rowCount+2,rowCount*2+1],
-    [1,rowCount,rowCount+1,rowCount*2+1]
+    [0,rowCount,rowCount+1,rowCount*2],
+    [0,1,2,rowCount+1],
+    [1,rowCount,rowCount+1,rowCount*2+1],
+    [1,rowCount,rowCount+1,rowCount+2]
 ];
-
 const oTetromino = [
     [0,1,rowCount,rowCount+1],
     [0,1,rowCount,rowCount+1],
@@ -29,15 +46,8 @@ const oTetromino = [
     [0,1,rowCount,rowCount+1]
 ];
 
-const iTetromino = [
-    [1,rowCount+1,rowCount*2+1,rowCount*3+1],
-    [rowCount,rowCount+1,rowCount+2,rowCount+3],
-    [1,rowCount+1,rowCount*2+1,rowCount*3+1],
-    [rowCount,rowCount+1,rowCount+2,rowCount+3]
-];
-
-const tetrominos = [ lTetromino, zTetromino, tTetromino, oTetromino, iTetromino ];
-const classes = [ 'l', 'z', 't', 'o', 'i' ];
+const tetrominos = [ lTetromino, jTetromino, zTetromino, sTetromino, iTetromino, tTetromino, oTetromino ];
+const classes = [ 'l', 'j', 'z', 's', 'i', 't', 'o' ];
 
 const DEFAULT_TIME = 600;
 let TIME = DEFAULT_TIME; // cada "TIME"ms se mueve la figura una línea hacia abajo
@@ -47,7 +57,9 @@ let currentTetrominoPositions = [];
 let gridSquares = [];
 let timerId;
 let gameStarted = false;
+let doRestartGame = false;
 let paused = false;
+let gameOver = false;
 let currentTetrominoRandomIdx = -1;
 let nextTetrominoRandomIdx = -1;
 let score = 0;
@@ -56,19 +68,16 @@ let volumeOn = true;
 const grid = document.querySelector('.grid');
 const scoreDisplay = document.querySelector('#score');
 const audio = document.getElementById('audio');
-audio.loop = true;
+const audioSrc = document.querySelector('#audio source');
 
 document.addEventListener('DOMContentLoaded', () => {
-    gridSquares = Array.from( document.querySelectorAll('.grid div:not(.before__grid)') ); 
-    keyboardControl();
-
     document.addEventListener('swiped-left', () => {
-        if( !paused ){
+        if( !paused && !gameOver ){
             rotatePosition();
         }
     });
     document.addEventListener('swiped-right', () => {
-        if( !paused ){
+        if( !paused && !gameOver ){
             rotatePosition();
         }
     });
@@ -82,9 +91,7 @@ const newTetromino = () => {
     nextTetrominoRandomIdx = Math.floor(Math.random()*tetrominos.length);
     drawTetromino( true );
     displayNextTetromino();
-    timerId = setInterval( () => {
-        moveDown();
-    }, TIME );
+    timerId = setInterval( moveDown, TIME );
 };
 
 const drawTetromino = ( draw ) => {
@@ -104,11 +111,13 @@ const drawTetromino = ( draw ) => {
 const displayNextTetromino = () => {
     const displayRowCount = 4;
     const displayTetrominos = [
-        [1, displayRowCount+1, displayRowCount*2+1, 2],
-        [0,displayRowCount,displayRowCount+1,displayRowCount*2+1],
-        [1,displayRowCount,displayRowCount+1,displayRowCount+2],
-        [0,1,displayRowCount,displayRowCount+1],
-        [1,displayRowCount+1,displayRowCount*2+1,displayRowCount*3+1]
+        [0, displayRowCount, displayRowCount*2, displayRowCount*2+1],
+        [1,displayRowCount+1,displayRowCount*2+1,displayRowCount*2],
+        [0,1,displayRowCount+1,displayRowCount+2],
+        [1,2,displayRowCount,displayRowCount+1],
+        [1,displayRowCount+1,displayRowCount*2+1,displayRowCount*3+1],
+        [0,displayRowCount,displayRowCount+1,displayRowCount*2],
+        [0,1,displayRowCount,displayRowCount+1]
     ];
     const displayGridSquares = Array.from(document.querySelectorAll('.next-figure-grid div'));
     for( const square of displayGridSquares ){
@@ -141,24 +150,21 @@ const freeze = () => {
         return gridSquares[currentPosition + tetroMinoPosition + rowCount].classList.contains('taken');
     });
     if( contains ){
+        clearInterval(timerId);
         for( const pos of currentTetrominoPositions ){
             gridSquares[currentPosition + pos].classList.add('taken');
         }
         checkline();
-        clearInterval(timerId);
         newTetromino();
+        checkGameOver();
     }
     return contains;
 };
 
-const checkValidPositionLeft = ( forRotation ) => {
+const checkValidPositionLeft = () => {
     // Indica si alguna de las celdas del tetromino está en la primera columna del grid
     const isAtLeftEdge = currentTetrominoPositions.some( (tetrominoPosition) => {
-        if( forRotation ){
-            return (currentPosition + tetrominoPosition)%rowCount < 0;
-        } else{
-            return (currentPosition + tetrominoPosition)%rowCount == 0;
-        }
+        return (currentPosition + tetrominoPosition)%rowCount == 0;
     });
     // ¿Tiene un tetromino a la izquierda?
     const hasTetrominoNext = currentTetrominoPositions.some( (tetroMinoPosition) => {
@@ -167,14 +173,10 @@ const checkValidPositionLeft = ( forRotation ) => {
     return !isAtLeftEdge && !hasTetrominoNext;
 };
 
-const checkValidPositionRight = ( forRotation ) => {
+const checkValidPositionRight = () => {
     // ¿Tiene alguna pieza en la pared de la derecha?
     const isAtRightEdge = currentTetrominoPositions.some( (tetrominoPosition) => {
-        if( forRotation ){
-            return (currentPosition + tetrominoPosition)%rowCount == rowCount;
-        } else{
-            return (currentPosition + tetrominoPosition)%rowCount == rowCount -1;
-        }
+        return (currentPosition + tetrominoPosition)%rowCount == rowCount -1;
     });
     // ¿Tiene un tetromino a la derecha?
     const hasTetrominoNext = currentTetrominoPositions.some( (tetroMinoPosition) => {
@@ -191,12 +193,12 @@ const checkValidPositionBotom = () => {
 };
 
 const checkValidRotation = () => {
-    return checkValidPositionLeft( true ) && checkValidPositionRight( true ) && checkValidPositionBotom();
+    return checkValidPositionLeft() && checkValidPositionRight() && checkValidPositionBotom();
 };
 
 const moveLeft = () =>{
     drawTetromino( false ); 
-    if( checkValidPositionLeft( false ) ){
+    if( checkValidPositionLeft() ){
         currentPosition -= 1;
     }
     drawTetromino( true ); 
@@ -204,7 +206,7 @@ const moveLeft = () =>{
 
 const moveRight = () => {
     drawTetromino( false );
-    if( checkValidPositionRight( false ) ){
+    if( checkValidPositionRight() ){
         currentPosition += 1;
     }
     drawTetromino( true );
@@ -227,19 +229,19 @@ const rotatePosition = () => {
 
 const keyboardControl = () => {
     document.addEventListener( 'keyup', (e) => {
-        if( e.keyCode === 37 && !paused ){ // Flecha izquierda
+        if( e.keyCode === 37 && !paused && !gameOver ){ // Flecha izquierda
             moveLeft();
         }
-        if( e.keyCode === 39 && !paused ){ // Flecha derecha
+        if( e.keyCode === 39 && !paused && !gameOver ){ // Flecha derecha
             moveRight();
         }
-        if( e.keyCode == 38 && !paused ){ // Flecha superior
+        if( e.keyCode == 38 && !paused && !gameOver ){ // Flecha superior
             rotatePosition();
         }
-        if( e.keyCode === 40 && !paused ){ // Flecha inferior
+        if( e.keyCode === 40 && !paused && !gameOver ){ // Flecha inferior
             moveDown();
         }
-        if( e.keyCode === 32 && !paused ){ // Espacio
+        if( e.keyCode === 32 && !paused && !gameOver ){ // Espacio
             startPause();
         }
     });
@@ -247,14 +249,20 @@ const keyboardControl = () => {
 
 const startPause = () => {
     if( !gameStarted ){
-        gameStarted = true;
+        audioSrc.src = 'assets/Tetris.mp3';
+        audio.loop = true;
+        audio.load();
+        if( doRestartGame ){
+            restartGame();
+        } else{
+            startGame();
+        }
         newTetromino();
     } else{
+        document.querySelector('#pause-screen').classList.toggle('visible');
         if( paused ){
             paused = false;
-            timerId = setInterval( () => {
-                moveDown();
-            }, TIME );
+            timerId = setInterval( moveDown, TIME );
         } else{
             paused = true;
             clearInterval(timerId);
@@ -269,7 +277,7 @@ const startPause = () => {
 };
 
 const togglePlayPauseIcon = ( isStart ) => {
-    const startButtonIcon = document.querySelector('#start-button i[class~="material-icons"]');
+    const startButtonIcon = document.querySelector('#pause-button i[class~="material-icons"]');
     if( isStart ){
         startButtonIcon.innerHTML = 'pause';
     } else{
@@ -278,7 +286,7 @@ const togglePlayPauseIcon = ( isStart ) => {
 };
 
 const checkline = () => {
-    for( let i = 0; i < 199; i+=10 ){
+    for( let i = 0; i < 200; i+=10 ){
         const row = gridSquares.slice(i, i+10);
         // Toda una fila tiene la clase 'taken' => hemos hecho línea
         const isLine = row.every( (square) => {
@@ -296,6 +304,11 @@ const checkline = () => {
             }
             // Incrementamos la puntuación
             score += 10;
+            if( score%50 == 0 && TIME > 100 ){ // Cada 50 puntos aumentamos la velocidad
+                TIME -= 100;
+                clearInterval(timerId);
+                timerId = setInterval( moveDown, TIME );
+            }
             scoreDisplay.innerHTML = score;
             // Reseteamos el array de cuadrados
             gridSquares = Array.from(document.querySelectorAll('.grid div:not(.before__grid)'));
@@ -304,16 +317,59 @@ const checkline = () => {
 };
 
 const setVolume = () => {
-    const volumeButtonIconEl = document.querySelector('#volume-button i');
-    if( volumeOn ){
-        volumeButtonIconEl.innerHTML = 'volume_mute';
-    } else{
-        volumeButtonIconEl.innerHTML = 'volume_up';
+    const volumeButtonIconEls = document.querySelectorAll('.volume-button i');
+    for( const volumeButtonIconEl of volumeButtonIconEls ){
+        if( volumeOn ){
+            volumeButtonIconEl.innerHTML = 'volume_mute';
+        } else{
+            volumeButtonIconEl.innerHTML = 'volume_up';
+        }
     }
     volumeOn = !volumeOn;
     if( volumeOn && !paused ){
-        this.audio.play();
+        audio.play();
     } else if( !paused ) {
-        this.audio.pause();
+        audio.pause();
     }
+};
+
+const checkGameOver = () => {
+    gameOver = currentTetrominoPositions.some( (pos) => {
+        return gridSquares[pos+currentPosition].classList.contains('taken');
+    });
+    if( gameOver ){
+        gameStarted = false;
+        doRestartGame = true;
+        document.querySelector('#game-over-screen').classList.add('visible');
+        clearInterval(timerId);
+        audio.pause();
+        audioSrc.src = 'assets/gameover.wav';
+        audio.loop = false;
+        audio.load();
+        audio.play();
+    }
+};
+
+const startGame = () => {
+    let gridSquareEls = document.querySelectorAll('.grid div:not(.before__grid)');
+    gridSquares = Array.from( gridSquareEls ); 
+    keyboardControl();
+    document.querySelector('#start-game-screen').classList.add('invisible');
+    gameStarted = true;
+};
+
+const restartGame = () => {
+    TIME = DEFAULT_TIME;
+    let gridSquareEls = document.querySelectorAll('.grid div:not(.before__grid)');
+    // Limpiar el grid
+    for( let i = 0; i < 200; i++ ){
+        gridSquareEls[i].className = '';
+    }
+    gridSquares = Array.from( gridSquareEls ); 
+    document.querySelector('#game-over-screen').classList.remove('visible');
+    score = 0;
+    scoreDisplay.innerHTML = score;
+    doRestartGame = false;
+    gameOver = false;
+    gameStarted = true;
 };
